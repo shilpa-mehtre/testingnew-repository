37fc48221453386d16eb7c5d0ec9ca7de87c794f
Shilpa <Shilpa@DESKTOP-NKL4O8F>
2024-04-02 10:48:25 +0530

    shilpa new repo

--- Git-create-new-repository/src/Test.java
+++ Git-create-new-repository/src/Test.java
@@ -3,9 +3,12 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("new repo");
 
-		system.out.println("new clone");
+	
+		
+			System.out.println("new clone");
+		
 
-		system.out.println("hi");
+		
 		
 		
 	}
